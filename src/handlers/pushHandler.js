59a2b882fd2b9b8329f1610169d781d0a4c2768b
Filip Kulevski <filipkule@gmail.com>
2020-10-14 21:15:42 +0200

    Inital commit for push handler

---
+++ src/handlers/pushHandler.js
@@ -0,0 +1,44 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const { WebhookClient, MessageEmbed } = require('discord.js')
+const { extractDataFromWebhookUrl } = require('../helpers')
+
+module.exports = ({ webhookUrl }) => {
+    const { payload, eventName } = github.context
+
+    // if (eventName !== 'push') {
+    //     console.warn('push handler can be executed only on "push" action triggers')
+    //     return Promise.resolve()
+    // }
+
+    // const { id, token } = extractDataFromWebhookUrl(webhookUrl)
+    // const client = new WebhookClient(id, token)
+    
+    // let embed = createEmbed(data)
+    // return client.send(embed).then(result => {
+    //     client.destroy()
+    //     return data
+    // }).catch(error => {
+    //     client.destroy()
+    //     throw error
+    // })
+    console.log(JSON.stringify(payload), eventName)
+    return Promise.resolve()
+}
+
+
+function createEmbed({ action, name, body, tag, url, draft, prerelease, published, branch }) {
+    let embed = new MessageEmbed({ type: 'rich' })
+    embed.setColor(prerelease ? 0xf66a0a : 0x28a745)
+    embed.setTitle(`${prerelease ? 'Pre-release' : 'Release'}: ${tag} ${name} ${draft ? '(Draft)': ''}`)
+    embed.setURL(url)
+    embed.setDescription(`${trimBody(body)}`)
+    embed.setFooter(`Branch: ${branch}`)
+    embed.setTimestamp(new Date(published))
+    return embed
+}
+
+function trimBody(body = '') {
+    if (body.length < 2000) return body
+    return `${body.substring(0, 2000)}...`
+}
