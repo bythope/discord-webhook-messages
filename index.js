b917490bc47bd6623973879ecede4d8b4f74eb86
kalevski <kalevski@outlook.com>
2020-10-14 15:17:02 +0200

    added error handler

--- index.js
+++ index.js
@@ -16,4 +16,6 @@ if (!isWebhookUrl(input.webhookUrl)) {
 axios.post(input.webhookUrl, { content: '', embeds: [ {
     title: 'raw payload',
     description: JSON.stringify(payload)
-}] })
+}] }).catch(error => {
+    console.error(error)
+})
